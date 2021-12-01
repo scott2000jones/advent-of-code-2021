2802231abd18fabcdc8db763c527b02f4ab8a4cf
scott2000jones <scott2000jones@gmail.com>
2021-12-01 14:01:56 +0000

    Day 1 solved

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-#Advent of Code 2021
+# Advent of Code 2021
 It's in Rust!
--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,40 @@
+use std::fs::*;
+
+fn part1(values: &Vec<u32>) -> u32{
+    let mut increase_count : u32 = 0;
+    let mut prev : u32 = u32::MIN;
+    for current in values {
+        // println!("Comparing {} to prev {} ", current, prev);
+        if *current > prev && prev > u32::MIN {
+            increase_count += 1;
+        }
+        prev = *current;
+    }
+    return increase_count;
+}
+
+fn part2(values: &Vec<u32>) -> u32 {
+    let mut increase_count : u32 = 0;    
+    let mut prev_sum : u32 = u32::MIN;
+    for w in values.windows(3) {
+        let new_sum = w.iter().sum();
+        // println!("{}", new_sum);
+        if new_sum > prev_sum && prev_sum > u32::MIN {
+            increase_count += 1;
+            // println!("  increased!");
+        }
+        prev_sum = new_sum;
+    }
+    return increase_count;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input_file = read_to_string("input.txt")
+        .expect("Cannot read the file");
+    let values: Vec<u32> = input_file.lines()
+        .map(|line| line.parse().expect("Cannot parse"))
+        .collect();
+
+    println!("Part1 result: {} increases", part1(&values));
+    println!("Part2 result: {} increases", part2(&values));
 }
