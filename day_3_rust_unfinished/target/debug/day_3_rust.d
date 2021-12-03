6f0df6a6dbe332f219ad117766e3d0b85c481db8
scott2000jones <scott2000jones@gmail.com>
2021-12-03 16:41:52 +0000

    Removed unfinished Rust day 3 code

--- day_3_rust_unfinished/target/debug/day_3_rust.d
+++
@@ -1,1 +0,0 @@
-/home/scott/advent-of-code-2021/day_3_rust/target/debug/day_3_rust: /home/scott/advent-of-code-2021/day_3_rust/src/main.rs
--- day_3_rust_unfinished/target/debug/deps/day_3_rust-947b8ac03166712f.d
+++
@@ -1,5 +0,0 @@
-/home/scott/advent-of-code-2021/day_3_rust/target/debug/deps/day_3_rust-947b8ac03166712f: src/main.rs
-
-/home/scott/advent-of-code-2021/day_3_rust/target/debug/deps/day_3_rust-947b8ac03166712f.d: src/main.rs
-
-src/main.rs:
